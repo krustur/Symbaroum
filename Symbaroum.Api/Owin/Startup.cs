86b868e80ac5582a66294ce55d581cb7500b396d
Krister Jansson <krustur@gmail.com>
2017-04-02 23:51:56 +0200

    Added some entity framework hello world code

--- Symbaroum.Api/Owin/Startup.cs
+++ Symbaroum.Api/Owin/Startup.cs
@@ -10,7 +10,7 @@ namespace Symbaroum.Api.Owin
         public void Configuration(IAppBuilder appBuilder)
         {
             // Configure Web API for self-host. 
-            HttpConfiguration config = new HttpConfiguration();
+            var config = new HttpConfiguration();
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
--- Symbaroum.Tests/UnitTest1.cs
+++ Symbaroum.Tests/UnitTest1.cs
@@ -1,5 +1,11 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿//using System;
+
+using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Symbaroum.Core.Common.Database;
+using System.Data.Entity;
+using System.Linq;
 
 namespace Symbaroum.Tests
 {
@@ -9,7 +15,7 @@ namespace Symbaroum.Tests
         [AssemblyInitialize]
         public static void Configure(TestContext testContext)
         {
-            LocalDbHelper.GetLocalDb("testdb", deleteIfExists: true);
+            //LocalDbHelper.GetLocalDb("Symbaroum", deleteIfExists: true);
         }
         
     }
@@ -17,9 +23,71 @@ namespace Symbaroum.Tests
     [TestClass]
     public class UnitTest1
     {
+        public class BloggingContext : DbContext
+        {
+            public BloggingContext()
+                : base(@"Server=(localdb)\SymbLocalDb;Database=Symbaroum;Integrated Security=True;")
+            {
+                Database.SetInitializer(new DropCreateDatabaseAlways<BloggingContext>());
+            }
+
+            public DbSet<Blog> Blogs { get; set; }
+            public DbSet<Post> Posts { get; set; }
+
+            protected override void OnModelCreating(DbModelBuilder modelBuilder)
+            {
+                modelBuilder.Entity<Post>()
+                    .Property(u => u.Content)
+                    .HasColumnName("da content");
+            }
+        }
+
+        public class Blog
+        {
+            public int BlogId { get; set; }
+            public string Name { get; set; }
+
+            public virtual List<Post> Posts { get; set; }
+        }
+
+        public class Post
+        {
+            public int PostId { get; set; }
+            public string Title { get; set; }
+            public string Content { get; set; }
+
+            public int BlogId { get; set; }
+            public virtual Blog Blog { get; set; }
+        }
+
         [TestMethod]
         public void TestMethod1()
         {
+            using (var db = new BloggingContext())
+            {
+                // Create and save a new Blog 
+                //Console.Write("Enter a name for a new Blog: ");
+                //var name = Console.ReadLine();
+                var name = "Krister";
+
+                var blog = new Blog { Name = name };
+                db.Blogs.Add(blog);
+                db.SaveChanges();
+
+                // Display all Blogs from the database 
+                var query = from b in db.Blogs
+                            orderby b.Name
+                            select b;
+
+                Console.WriteLine("All blogs in the database:");
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.Name);
+                }
+
+                Console.WriteLine("Press any key to exit...");
+                //Console.ReadKey();
+            }
         }
     }
 }
