607507551c594f077c0fbfa576cb96b70f4eb773
Krister Jansson <krustur@gmail.com>
2017-05-07 15:53:28 +0200

    Added broken test

--- Symbaroum.Tests/IntegrationTest.cs
+++ Symbaroum.Tests/IntegrationTest.cs
@@ -1,118 +1,114 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Data.Entity;
-//using System.Data.Entity.Infrastructure;
-//using System.Data.Entity.Migrations;
-//using System.Data.SqlClient;
-//using System.Transactions;
-//using Autofac;
-//using Microsoft.VisualStudio.TestTools.UnitTesting;
-//using Symbaroum.Core.Common;
-//using Symbaroum.Core.Common.Database;
-//using Symbaroum.Core.Infrastructure;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-//namespace Symbaroum.Tests
-//{
-//[TestClass]
-//public class IntegrationTestsBase
-//{
-//    private TransactionScope _transactionScope;
+namespace Symbaroum.Tests
+{
+    //[TestClass]
+    //public class IntegrationTestsBase
+    //{
+    //    private TransactionScope _transactionScope;
 
-//    [TestInitialize]
-//    public void Initialize()
-//    {
-//        _transactionScope = new TransactionScope();
-//    }
+    //    [TestInitialize]
+    //    public void Initialize()
+    //    {
+    //        _transactionScope = new TransactionScope();
+    //    }
 
-//    [TestCleanup]
-//    public void TestCleanup()
-//    {
-//        Transaction.Current.Rollback();
-//        _transactionScope.Dispose();
-//    }
-//}
+    //    [TestCleanup]
+    //    public void TestCleanup()
+    //    {
+    //        Transaction.Current.Rollback();
+    //        _transactionScope.Dispose();
+    //    }
+    //}
 
-//    [TestClass]
-//    public class IntegrationTest
-//    {
-//        private string _connectionString;
+    [TestClass]
+    public class IntegrationTest
+    {
+        //        private string _connectionString;
 
-//        [TestInitialize]
-//        public void Arrange()
-//        {
-//            var configReader = Bootstrap.Container.Resolve<IConfigReader>();
-//            _connectionString = configReader.ConnectionString;
-//        }
+        [TestInitialize]
+        public void Arrange()
+        {
+            //var configReader = Bootstrap.Container.Resolve<IConfigReader>();
+            //_connectionString = configReader.ConnectionString;
+        }
 
-//        // you don't want any of these executed automatically
-//        [TestMethod]
-//        //[Ignore] // Only for manual execution
-//        public void Wipe_And_Create_Database()
-//        {
-//            // drop database first
-//            LocalDbHelper.DropDatabase(_connectionString);
-//            var domainContextFactory = Bootstrap.Container.Resolve<IDomainContextFactory>();
-//            using (var domainContext = domainContextFactory.Create())
-//            {
-//                LocalDbHelper.ForceInitialization(domainContext);
-//            }
+        [TestMethod]
+        public void AlwaysFalse()
+        {
+            Assert.IsTrue(false);
+        }
 
-//            // And after the DB is created, you can put some initial base data 
-//            // for your tests to use
-//            // usually this data represents lookup tables, like Currencies, Countries, Units of Measure, etc
-//            using (var domainContext = domainContextFactory.Create())
-//            {
-//                Console.WriteLine("Seeding test data into database");
-//                // discussion for that to follow
+        //        // you don't want any of these executed automatically
+        //        [TestMethod]
+        //        //[Ignore] // Only for manual execution
+        //        public void Wipe_And_Create_Database()
+        //        {
+        //            // drop database first
+        //            LocalDbHelper.DropDatabase(_connectionString);
+        //            var domainContextFactory = Bootstrap.Container.Resolve<IDomainContextFactory>();
+        //            using (var domainContext = domainContextFactory.Create())
+        //            {
+        //                LocalDbHelper.ForceInitialization(domainContext);
+        //            }
 
-//                //SeedContextForTests.Seed(domainContext);
+        //            // And after the DB is created, you can put some initial base data 
+        //            // for your tests to use
+        //            // usually this data represents lookup tables, like Currencies, Countries, Units of Measure, etc
+        //            using (var domainContext = domainContextFactory.Create())
+        //            {
+        //                Console.WriteLine("Seeding test data into database");
+        //                // discussion for that to follow
 
-//                Console.WriteLine("Seeding test data is complete");
-//            }
-//        }
+        //                //SeedContextForTests.Seed(domainContext);
+
+        //                Console.WriteLine("Seeding test data is complete");
+        //            }
+        //        }
 
 
 
-//        // this method is only updates your DB to latest migration.
-//        // does the same as if you run "Update-Database" in nuget console in Visual Studio
-//        [TestMethod]
-//        [Ignore] // Only for manual execution
-//        public void Update_Database()
-//        {           
-//            var migrationConfiguration = new DomainContextMigrationsConfiguration();
+        //        // this method is only updates your DB to latest migration.
+        //        // does the same as if you run "Update-Database" in nuget console in Visual Studio
+        //        [TestMethod]
+        //        [Ignore] // Only for manual execution
+        //        public void Update_Database()
+        //        {           
+        //            var migrationConfiguration = new DomainContextMigrationsConfiguration();
 
-//            migrationConfiguration.TargetDatabase = new DbConnectionInfo(_connectionString, "System.Data.SqlClient");
+        //            migrationConfiguration.TargetDatabase = new DbConnectionInfo(_connectionString, "System.Data.SqlClient");
 
-//            var migrator = new DbMigrator(migrationConfiguration);
+        //            var migrator = new DbMigrator(migrationConfiguration);
 
-//            migrator.Update();
-//        }
+        //            migrator.Update();
+        //        }
 
-//        [TestMethod]
-//        public void TestMethod1()
-//        {
-//            //using (var db = new DomainContext())
-//            //{
-//            //    // Create and save a new Blog 
-//            //    var name = "Krister";
+        //        [TestMethod]
+        //        public void TestMethod1()
+        //        {
+        //            //using (var db = new DomainContext())
+        //            //{
+        //            //    // Create and save a new Blog 
+        //            //    var name = "Krister";
 
-//            //    var blog = new Blog { Name = name };
-//            //    db.Blogs.Add(blog);
-//            //    db.SaveChanges();
+        //            //    var blog = new Blog { Name = name };
+        //            //    db.Blogs.Add(blog);
+        //            //    db.SaveChanges();
 
-//            //    // Display all Blogs from the database 
-//            //    var query = from b in db.Blogs
-//            //                orderby b.Name
-//            //                select b;
+        //            //    // Display all Blogs from the database 
+        //            //    var query = from b in db.Blogs
+        //            //                orderby b.Name
+        //            //                select b;
 
-//            //    Console.WriteLine("All blogs in the database:");
-//            //    foreach (var item in query)
-//            //    {
-//            //        Console.WriteLine(item.Name);
-//            //    }
+        //            //    Console.WriteLine("All blogs in the database:");
+        //            //    foreach (var item in query)
+        //            //    {
+        //            //        Console.WriteLine(item.Name);
+        //            //    }
 
-//            //    Console.WriteLine("Press any key to exit...");
-//            //}
-//        }
-//    }
-//}
+        //            //    Console.WriteLine("Press any key to exit...");
+        //            //}
+        //        }
+        //    }
+    }
+}
